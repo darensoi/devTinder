e3acda5246084df47348672184ad19b015846438
darensoi <daren.soibam@gmail.com>
2025-09-24 23:59:08 +0530

    created editProfile and changePassword apis

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,4 +60,11 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null , {
+    expires: new Date(Date.now())
+  })
+  res.send("Logged out successfully!!!");
+});
+
 module.exports = authRouter;
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,12 +1,65 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
+const { validateEditProfileData } = require('../utils/validation');
 const profileRouter = express.Router();
+const bcrypt = require("bcrypt");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
+    console.log("Inside profile view", req);
+    
     const user = req.user;
     res.send(user);
   } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key]
+    });
+
+    await loggedInUser.save();
+
+    res.json({
+      message: `Profile updated successfully for ${loggedInUser.firstName}`,
+      data: loggedInUser,
+    })
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/changePassword", userAuth, async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+      throw new Error("Both old and new passwords are required");
+    }
+
+    const loggedInUser = req.user;
+
+    const isPasswordValid = await loggedInUser.validatePassword(oldPassword);
+    if (!isPasswordValid) {
+      throw new Error("Old password is incorrect");
+    }
+
+    loggedInUser.password = await bcrypt.hash(newPassword, 10);
+    await loggedInUser.save();
+
+    res.send("Password changed successfully for " + loggedInUser.firstName);
+  }
+  catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
 });
