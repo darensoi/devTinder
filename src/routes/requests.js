76b0103887bd257e08dc48a02fa458ab407bac92
darensoi <daren.soibam@gmail.com>
2026-02-01 10:58:16 +0530

    added review requested connections api

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -26,18 +26,18 @@ requestsRouter.post("/sendConnectionRequest/:status/:toUserId", userAuth, async 
         { fromUserId: toUserId, toUserId: fromUserId }
       ]
     });
-    
-    const connectionRequest = new ConnectionRequest({
-      fromUserId,
-      toUserId,
-      status,
-    });
 
     if (existingConnectionRequest) {
       return res
         .status(400)
         .send({ error: "Connection request already exists between these users." });
     }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
 
     const data = await connectionRequest.save();
 
@@ -51,4 +51,36 @@ requestsRouter.post("/sendConnectionRequest/:status/:toUserId", userAuth, async 
   }
 });
 
+requestsRouter.post("/reviewRequest/:status/:requestedId", userAuth, async (req, res) => {
+  try {
+    const { status, requestedId } = req.params;
+    const loggedInUser = req.user;
+    
+    const allowedStatuses = ["accepted", "rejected"];
+    if (!allowedStatuses.includes(status)) {
+      return res.status(400).json({ error: "Invalid status value " + status });
+    }
+
+    const connectionRequest = await ConnectionRequest.findOne({
+      _id: requestedId,
+      toUserId: loggedInUser._id,
+      status: "interested",
+    });
+
+    if (!connectionRequest) {
+      return res
+        .status(404)
+        .send({ error: "No connection request found to review." });
+    }
+
+    connectionRequest.status = status;
+
+    const data = await connectionRequest.save();
+
+    res.json({message: "Connection Request " + status, data});
+    
+  } catch (error) {
+    res.status(400).send("Error in reviewing connection request: " + error.message);
+  }  
+})
 module.exports = requestsRouter;
